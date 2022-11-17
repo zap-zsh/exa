e206983dc3c1fdae98871f52848defa7a513f7ae
Nesbeer <76590320+ItzNesbroDev@users.noreply.github.com>
2022-11-17 14:42:41 +0530

    chore: made tree alias simple

--- exa.plugin.zsh
+++ exa.plugin.zsh
@@ -2,5 +2,5 @@
 
 if command -v exa &> /dev/null; then
   alias ls="exa --icons --group-directories-first"
-  alias tree="exa --icons --group-directories-first --tree --level=2"
+  alias tree="ls --tree --level=2"
 fi
