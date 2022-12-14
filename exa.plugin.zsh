c943fd0e57cf4f9eb861f92a6adb6050eccf466f
flo <flo-eberle@users.noreply.github.com>
2022-12-14 03:56:24 +0100

    added missing dash for option --tree (#4)
    Thanks for make the PR, this fix issue #5

--- exa.plugin.zsh
+++ exa.plugin.zsh
@@ -4,5 +4,5 @@ if command -v exa &> /dev/null; then
   alias ls='exa --group-directories-first --icons'
   alias ll='ls -lh --git'
   alias la='ll -a'
-  alias tree='ll -tree --level=2'
+  alias tree='ll --tree --level=2'
 fi
