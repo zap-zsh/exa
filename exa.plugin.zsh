2daa58fc759203d16fa9956a6e19eff01c8ca08b
Rob Wolinski <trekie86@gmail.com>
2023-04-18 16:16:55 -0700

    Add conditional statement to ll alias (#7)
    Exa does not support the git flag on all systems, namely Ubuntu. This
    change introduces a conditional statement in the alias declaration to
    only use the --git flag if git is enabled on exa.
    
    Ref: https://github.com/ogham/exa/issues/978

--- exa.plugin.zsh
+++ exa.plugin.zsh
@@ -8,6 +8,15 @@ fi
 
 # Create alias override commands using 'exa'
 alias ls='exa --group-directories-first --icons'
-alias ll='ls -lh --git'
+
+# Use the --git flag if the installed version of exa supports git
+# Related to https://github.com/ogham/exa/issues/978
+if exa --version | grep -q '+git';
+then
+	alias ll='ls -lh --git'
+else
+	alias ll='ls -lh'
+fi
+
 alias la='ll -a'
 alias tree='ll --tree --level=2'
